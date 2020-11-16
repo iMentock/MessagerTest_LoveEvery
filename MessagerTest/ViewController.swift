8ca725278ea0b49e4aeb110a547d6efb4f9ac1d2
Virgil Martinez <virgilmartinez@gmail.com>
2020-11-15 18:02:27 -0600

    Initial Commit

---
+++ MessagerTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessagerTest
+//
+//  Created by Virgil Martinez on 11/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
